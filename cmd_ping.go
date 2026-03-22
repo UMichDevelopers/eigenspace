39140f9eed0163579e6bf81d6bbd6d71a6fff228
Runxi Yu <runxiyu@umich.edu>
2026-03-22 10:01:59 +0000

    update

--- cmd_ping.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/bwmarrin/discordgo"
-
-func (b *bot) handlePingCommand(session *discordgo.Session, event *discordgo.MessageCreate, command *ParsedCommand) error {
-	return reply(session, event, "PONG")
-}
